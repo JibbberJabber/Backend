a617b60c2da08e4927bb432b9a829962c2a92689
IsabelaCeriani <56088703+IsabelaCeriani@users.noreply.github.com>
2022-06-13 22:57:30 -0300

    Update azureVm.sh

--- azureVm.sh
+++ azureVm.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 chmod 400 ${{ secrets.DEV_KEY }}
-ssh -i ${{ secrets.DEV_KEY }}  ${{ secrets.USERNAME }}@51.141.54.133
+ssh -i ${{ secrets.DEV_KEY }}  ${{ secrets.USERNAME }}@${{ secrets.DEV_IP }}
+cd Backend
+sudo docker-compose up --force-recreate
