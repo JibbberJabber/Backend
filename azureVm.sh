6d77ffc219cce1c0d2b92aa7b718fca0955c6a0b
IsabelaCeriani <isabela.ceriani@ing.austral.edu.ar>
2022-06-14 16:30:20 -0300

    changed azureVm.sh and RedeployVM.yml

--- azureVm.sh
+++ azureVm.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-chmod 400 ProdVM_key.pem
+chmod 400 ${{ secrets.PROD_KEY }}
 #ssh -i ProdVM_key.pem azureuser@20.90.74.161 'cd Backend & sudo docker-compose down & sudo docker-compose up -f --force-recreate -d'
 
-ssh -i ProdVM_key.pem azureuser@20.90.74.161 'cd Backend && sudo docker-compose down &&
-                                                            sudo docker pull
+ssh -i ${{ secrets.PROD_KEY }} ${{ secrets.USERNAME }}@${{ secrets.PROD_IP }} 'cd Backend && sudo docker-compose down &&
+                                                            sudo docker pull &&
                                                             sudo docker-compose up  -d'
