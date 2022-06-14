b2c4474636e3c9c0fb6e85cd131770735858832f
IsabelaCeriani <isabela.ceriani@ing.austral.edu.ar>
2022-06-13 23:29:09 -0300

    changed azureVm.sh values to prod vm ones

--- azureVm.sh
+++ azureVm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-chmod 400 ${{ secrets.DEV_KEY }}
-ssh -i ${{ secrets.DEV_KEY }}  ${{ secrets.USERNAME }}@${{ secrets.DEV_IP }}
+chmod 400 ${{ secrets.PROD_KEY }}
+ssh -i ${{ secrets.PROD_KEY }}  ${{ secrets.USERNAME }}@${{ secrets.PROD_IP }}
 cd Backend
 sudo docker-compose up --force-recreate
