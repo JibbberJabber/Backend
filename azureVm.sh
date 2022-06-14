3b2843a24606b6480bf3a1a6e7eb2e7a286e1530
IsabelaCeriani <56088703+IsabelaCeriani@users.noreply.github.com>
2022-06-14 15:25:27 -0300

    Update azureVm.sh

--- azureVm.sh
+++ azureVm.sh
@@ -3,5 +3,7 @@
 chmod 400 ${{ secrets.PROD_KEY }}
 ssh -i ${{ secrets.PROD_KEY }}  ${{ secrets.USERNAME }}@${{ secrets.PROD_IP }}
 cd Backend
+docker rm -f $(docker ps -qa)
+docker rmi -f $(docker images -aq)
 sudo docker-compose up --force-recreate -d
 
