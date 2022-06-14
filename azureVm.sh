fe93ca7d6a812eb7e40bf92a18cf0770a39c283c
IsabelaCeriani <56088703+IsabelaCeriani@users.noreply.github.com>
2022-06-13 22:12:17 -0300

    Update azureVm.sh

--- azureVm.sh
+++ azureVm.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-#chmod 400 SSHvmName.pem
-#ssh -i SSHvmName.pem  azureuser@51.141.54.133
+chmod 400 ${{ secrets.DEV_KEY }}
+ssh -i ${{ secrets.DEV_KEY }}  ${{ secrets.USERNAME }}@51.141.54.133
