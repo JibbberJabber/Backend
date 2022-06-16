96c46dfb7f682256f43864fb659c2ef845f65cb9
IsabelaCeriani <56088703+IsabelaCeriani@users.noreply.github.com>
2022-06-16 01:02:21 -0300

    Update azureVm.sh

--- azureVm.sh
+++ azureVm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 chmod 400 ${{ secrets.PROD_KEY }}
 ssh -i ${{ secrets.PROD_KEY }}  ${{ secrets.USERNAME }}@${{ secrets.PROD_IP }} 
-'cd Backend && sudo docker-compose down &&
- sudo docker pull &&
- sudo docker-compose up  -d'
+# 'cd Backend && sudo docker-compose down &&
+#  sudo docker pull &&
+#  sudo docker-compose up  -d'
