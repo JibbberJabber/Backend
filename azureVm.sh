258deaaf7c8aa215f3f0b5cc1d94be44d1a04e15
IsabelaCeriani <56088703+IsabelaCeriani@users.noreply.github.com>
2022-06-16 01:18:48 -0300

    Update azureVm.sh

--- azureVm.sh
+++ azureVm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-chmod 400 ${{ secrets.PROD_KEY }}
+chmod 400 ${{ secrets.A }}
 
 # chmod 400 ${{ secrets.PROD_KEY }}
 # ssh -i ${{ secrets.PROD_KEY }}  ${{ secrets.USERNAME }}@${{ secrets.PROD_IP }} 
