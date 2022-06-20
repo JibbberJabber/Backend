aa83f27b58b26747f81925ab2b45e9129b494142
IsabelaCeriani <56088703+IsabelaCeriani@users.noreply.github.com>
2022-06-20 14:38:03 -0300

    removed | in RedeployVM.yml

--- azureVm.sh
+++ azureVm.sh
@@ -3,7 +3,7 @@
 ${{ secrets.USERNAME }}
 
 chmod 400 ${{ secrets.PROD_KEY }} 
-ssh -i ${{ secrets.PROD_KEY }}  ${{ secrets.USERNAME }}@${{ secrets.PROD_IP }} 'cd Backend && sudo docker-compose down &&
+ssh -i ${{ secrets.PROD_KEY }}  ${{ secrets.USERNAME }}@${{ secrets.PROD_IP }} "cd Backend && sudo docker-compose down &&
  sudo docker pull &&
- sudo docker-compose up  -d'
+ sudo docker-compose up  -d"
 
