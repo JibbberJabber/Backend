0f4e0731bd2cac39b357bd1795d214a461cc4bf2
IsabelaCeriani <56088703+IsabelaCeriani@users.noreply.github.com>
2022-06-20 14:39:52 -0300

    removed '' in azureVm.sh

--- azureVm.sh
+++ azureVm.sh
@@ -3,7 +3,11 @@
 ${{ secrets.USERNAME }}
 
 chmod 400 ${{ secrets.PROD_KEY }} 
-ssh -i ${{ secrets.PROD_KEY }}  ${{ secrets.USERNAME }}@${{ secrets.PROD_IP }} "cd Backend && sudo docker-compose down &&
- sudo docker pull &&
- sudo docker-compose up  -d"
+ssh -i ${{ secrets.PROD_KEY }}  ${{ secrets.USERNAME }}@${{ secrets.PROD_IP }}
+cd Backend && sudo docker-compose down
+sudo docker pull
+sudo docker-compose up  -d
+#'cd Backend && sudo docker-compose down &&
+# sudo docker pull &&
+# sudo docker-compose up  -d'
 
