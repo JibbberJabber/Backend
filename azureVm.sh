3ab2a9fe67878267b53f687a4a274424d4859bff
IsabelaCeriani <56088703+IsabelaCeriani@users.noreply.github.com>
2022-06-20 20:53:53 -0300

    replaced ${} with () for key referencing in azureVm.sh

--- azureVm.sh
+++ azureVm.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 
-chmod 400 ${{ secrets.PROD_KEY }} 
-ssh -i ${{ secrets.PROD_KEY }}  ${{ secrets.USERNAME }}@${{ secrets.PROD_IP }}
-cd Backend && sudo docker-compose down
-sudo docker pull
-sudo docker-compose up  -d
+chmod 400 $(secrets.PROD_KEY)
+ssh -i $(secrets.PROD_KEY)  $(secrets.USERNAME)@$(secrets.PROD_IP)'cd Backend && sudo docker-compose down &&
+                                                                   sudo docker pull  &&
+                                                                   sudo docker-compose up  -d'
+#cd Backend && sudo docker-compose down
+#sudo docker pull
+#sudo docker-compose up  -d
 #'cd Backend && sudo docker-compose down &&
 # sudo docker pull &&
 # sudo docker-compose up  -d'
