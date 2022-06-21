cfb368cbade243d35b29c144387ad4246139a12a
IsabelaCeriani <56088703+IsabelaCeriani@users.noreply.github.com>
2022-06-20 22:05:06 -0300

    removed () from references to env values in azureVm.sh

--- azureVm.sh
+++ azureVm.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-
-chmod 400 $(KEY)
-ssh -i $(KEY)  $(USERNAME)@$(IP) "cd Backend && sudo docker-compose down &&
+chmod 400 $KEY
+ssh -i $KEY  $USERNAME@$IP "cd Backend && sudo docker-compose down &&
                                                             sudo docker pull ghcr.io/jibbberjabber/posts-api:master &&
                                                             sudo docker pull ghcr.io/jibbberjabber/folllowing-api:master &&
                                                             sudo docker-compose up  -d"
 
+#chmod 400 SSHvmName.pem
+# #ssh -i ${{ secrets.PROD_KEY }}  ${{ secrets.USERNAME }}@${{ secrets.PROD_IP }} "cd Backend && sudo docker-compose down &&
+#                                                            sudo docker pull ghcr.io/jibbberjabber/posts-api:master &&
+#                                                            sudo docker pull ghcr.io/jibbberjabber/folllowing-api:master &&
+#                                                            sudo docker-compose up  -d"
 
-#ssh -i SSHvmName.pem azureuser@51.141.54.133 "cd Backend && sudo docker-compose down &&
+
+# ssh -i SSHvmName.pem azureuser@51.141.54.133 "cd Backend && sudo docker-compose down &&
 #                                                            sudo docker pull ghcr.io/jibbberjabber/posts-api:master &&
 #                                                            sudo docker pull ghcr.io/jibbberjabber/folllowing-api:master &&
 #                                                            sudo docker-compose up  -d"
