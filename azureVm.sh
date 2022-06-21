45289ff046d83ba0d85a2932ec806ca04e8db776
IsabelaCeriani <56088703+IsabelaCeriani@users.noreply.github.com>
2022-06-21 13:44:45 -0300

    Changed azure vm redeploy logic. Now using "Update server action" in RedeployVM.yml instead of running azureVm.sh file

--- azureVm.sh
+++ azureVm.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-${{ secrets.USERNAME }}
-
-chmod 400 ${{ secrets.PROD_KEY }} 
-ssh -i ${{ secrets.PROD_KEY }}  ${{ secrets.USERNAME }}@${{ secrets.PROD_IP }}
-cd Backend && sudo docker-compose down
-sudo docker pull
-sudo docker-compose up  -d
+#${{ secrets.USERNAME }}
+#
+#chmod 400 ${{ secrets.PROD_KEY }}
+#ssh -i ${{ secrets.PROD_KEY }}  ${{ secrets.USERNAME }}@${{ secrets.PROD_IP }}
+#cd Backend && sudo docker-compose down
+#sudo docker pull
+#sudo docker-compose up  -d
 #'cd Backend && sudo docker-compose down &&
 # sudo docker pull &&
 # sudo docker-compose up  -d'
