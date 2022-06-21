101fa9cd47795a5fd0140c2c08c7e001adfa4fb3
iceriani <iceriani@mulesoft.com>
2022-06-21 13:36:47 -0300

    azureVm.sh using secrets and vm cli commands between ""

--- azureVm.sh
+++ azureVm.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-chmod 400 $KEY
-ssh -i $KEY  $USERNAME@$IP "cd Backend && sudo docker-compose down &&
+chmod 400 ${{ secrets.DEV_KEY }}
+ssh -i ${{ secrets.DEV_KEY }}  ${{ secrets.USERNAME }}@${{ secrets.DEV_IP }} "cd Backend && sudo docker-compose down &&
                                                             sudo docker pull ghcr.io/jibbberjabber/posts-api:master &&
                                                             sudo docker pull ghcr.io/jibbberjabber/folllowing-api:master &&
                                                             sudo docker-compose up  -d"
 
-#chmod 400 SSHvmName.pem
-# #ssh -i ${{ secrets.PROD_KEY }}  ${{ secrets.USERNAME }}@${{ secrets.PROD_IP }} "cd Backend && sudo docker-compose down &&
+
+#ssh -i $KEY  $USERNAME@$IP "cd Backend && sudo docker-compose down &&
 #                                                            sudo docker pull ghcr.io/jibbberjabber/posts-api:master &&
 #                                                            sudo docker pull ghcr.io/jibbberjabber/folllowing-api:master &&
 #                                                            sudo docker-compose up  -d"
+
 
 
 # ssh -i SSHvmName.pem azureuser@51.141.54.133 "cd Backend && sudo docker-compose down &&
@@ -18,12 +19,3 @@ ssh -i $KEY  $USERNAME@$IP "cd Backend && sudo docker-compose down &&
 #                                                            sudo docker-compose up  -d"
 #
 #
-      #ssh -i ${{ secrets.PROD_KEY }}  ${{ secrets.USERNAME }}@${{ secrets.PROD_IP }}
-      #cd Backend && sudo docker-compose down
-      #sudo docker pull
-      #sudo docker-compose up  -d
-      #'cd Backend && sudo docker-compose down &&
-      # sudo docker pull &&
-      # sudo docker-compose up  -d'
-
-      #ssh -i SSHvmName.pem azureuser@51.141.54.133 "mkdir hello2"
